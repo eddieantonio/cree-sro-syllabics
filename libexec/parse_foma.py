1120f9aaab837d852b16b0e67db052b37e34cb5c
Eddie Antonio Santos <easantos@ualberta.ca>
2018-07-05 22:01:24 -0600

    Successfully parse FOMA, but I don't know what it means.

--- libexec/parse_foma.py
+++ libexec/parse_foma.py
@@ -16,7 +16,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# GLOBAL: sigma: (num, symbol) pairs.
 sigma = []
+states = []
+last_state_no = None
+
+
+class InvalidState(ValueError):
+    ...
 
 
 def read_header(line):
@@ -43,17 +50,68 @@ def read_sigma_start(line):
 
 
 def read_sigma_entry(line):
+    global sigma
     if line == '##states##':
-        return read_state
+        return read_state_array
 
     num_str, _, symbol_str = line.partition(' ')
     sigma.append((int(num_str), to_symbol(symbol_str)))
     return read_sigma_entry
 
 
-def read_state(line):
-    import pdb
-    pdb.set_trace()
+def is_sentinel_state(nums):
+    return all(n == -1 for n in nums)
+
+
+def numberify_line(state):
+    def wrapper(line):
+        nums = [int(c) for c in line.split()]
+        return state(nums)
+    return wrapper
+
+
+@numberify_line
+def read_state_array(nums):
+    global last_state_no
+
+    if is_sentinel_state(nums):
+        return read_end
+    elif len(nums) == 5:
+        state_no, in_, out, target, final_state = nums
+    elif len(nums) == 4:
+        state_no, in_, target, final_state = nums
+        out = in_
+    elif len(nums) == 3:
+        assert last_state_no is not None
+        in_, out, target = nums
+        state_no = last_state_no
+        final_state = None
+    elif len(nums) == 2:
+        assert last_state_no is not None
+        in_, target = nums
+        state_no = last_state_no
+        out = in_
+        final_state = None
+    else:
+        raise InvalidState
+
+    states.append(
+            (state_no, in_, in_, target, final_state)
+    )
+    last_state_no = state_no
+    return read_state_array
+
+
+def read_end(line):
+    assert line == '##end##'
+    return invalid_state
+
+
+def invalid_state(line):
+    """
+    Should never reach this state.
+    """
+    raise ValueError(line)
 
 
 class Symbol:
@@ -82,3 +140,6 @@ with open('Cans-to-Latn.txt') as fsm_file:
     next_state = read_header
     for line in fsm_file:
         next_state = next_state(line.rstrip())
+
+from pprint import pprint
+pprint(states)
