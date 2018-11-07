380af9cbe131a29a37a50d9b64b9d8d180c2c3a4
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-07 11:03:08 -0700

    Ignore lack of type stubs for py.test.

--- tests/test_syllabics_conversion.py
+++ tests/test_syllabics_conversion.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
+import pytest  # type: ignore
 
 from crk_orthography import sro2syllabics, syllabics2sro
 
